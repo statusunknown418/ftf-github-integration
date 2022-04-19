92ddda156be8da8ffe1f95527342d687aa7f7a98
alvaro <alvarodevcode@outlook.com>
2022-04-19 11:52:04 -0500

    feat: :zap: formatters and styling setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./pages/**/*.tsx', './ui/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        'text-primary': '#ffffff',
+        'bg-primary': '#18181b',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'system-ui'],
+      },
+    },
+  },
+  plugins: [],
+}
